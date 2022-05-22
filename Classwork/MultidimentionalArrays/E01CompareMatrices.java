c75fc055a162dfa4b74d173913e5dc5f47b6acb5
Daniel Davidov <danidavidov13.dd@gmail.com>
2022-05-22 10:01:01 +0300

    Solution updated
    Optimization of the code

--- Classwork/MultidimentionalArrays/E01CompareMatrices.java
+++ Classwork/MultidimentionalArrays/E01CompareMatrices.java
@@ -7,33 +7,8 @@ public class E01CompareMatrices {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        //read first matrix
-        int[] dimentions = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt).toArray();
-        int rows = dimentions[0];
-        int cols = dimentions[1];
-
-        int[][] firstMatrix = new int[rows][];
-        for (int i = 0; i < rows; i++) {
-            int[] row = Arrays.stream(scanner.nextLine().split("\\s+"))
-                    .mapToInt(Integer::parseInt).toArray();
-            firstMatrix[i] = row;
-        }
-
-        // read second matrix
-        dimentions = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .mapToInt(Integer::parseInt).toArray();
-        rows = dimentions[0];
-        cols = dimentions[1];
-
-
-
-        int[][] secondMatrix = new int[rows][];
-        for (int i = 0; i < rows; i++) {
-            int[] row = Arrays.stream(scanner.nextLine().split("\\s+"))
-                    .mapToInt(Integer::parseInt).toArray();
-            secondMatrix[i] = row;
-        }
+        int[][] firstMatrix = readMatrix(scanner);
+        int[][] secondMatrix = readMatrix(scanner);
 
         boolean result = checkIfEqual(firstMatrix, secondMatrix);
 
@@ -42,6 +17,20 @@ public class E01CompareMatrices {
         } else {
             System.out.println("not equal");
         }
+    }
+
+    private static int[][] readMatrix(Scanner scanner) {
+        String[] dimensions = scanner.nextLine().split("\\s+");
+        int rows = Integer.parseInt(dimensions[0]);
+        int cols = Integer.parseInt(dimensions[1]);
+
+        int[][] matrix = new int[rows][];
+        for (int i = 0; i < matrix.length; i++) {
+            int[] row = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .mapToInt(Integer::parseInt).toArray();
+            matrix[i] = row;
+        }
+        return matrix;
     }
 
     private static boolean checkIfEqual(int[][] firstMatrix, int[][] secondMatrix) {
